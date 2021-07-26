a85c98f35ddc529239ffcd1d6e45a14fd3b879c5
ekaterina <e.i.omelyashchik@gmail.com>
2021-07-26 15:06:00 +0200

    fix

--- src/test/java/ui/tests/PreconditionSteps.java
+++ src/test/java/ui/tests/PreconditionSteps.java
@@ -3,11 +3,16 @@ package ui.tests;
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Selenide.$;
+
 public class PreconditionSteps {
 
     @Step("Add product to shop cart")
     public static void addProduct(String product) {
         Selenide.open(product);
-        Selenide.$(".add-to-cart-button").click();
+        $(".add-to-cart-button").click();
+        $("#topcartlink").click();
+        $("#checkout").click();
+        $(".checkout-as-guest-button").click();
     }
 }
